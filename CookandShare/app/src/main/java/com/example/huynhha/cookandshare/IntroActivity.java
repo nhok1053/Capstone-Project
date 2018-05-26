0fc99aedb327741562c2704c36d04f4269004afb
DESKTOP-H31AHVD\LucTT <lucttse04138@fpt.edu.vn>
2018-05-26 17:11:13 +0700

    App intro

---
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/IntroActivity.java
@@ -0,0 +1,44 @@
+package com.example.huynhha.cookandshare;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        addSlide(IntroSlide.newInstance(R.layout.intro_slide_1));
+        addSlide(IntroSlide.newInstance(R.layout.intro_slide_2));
+        addSlide(IntroSlide.newInstance(R.layout.intro_slide_3));
+        addSlide(AppIntroFragment.newInstance("Title","Description",R.drawable.ic_done_white, Color.CYAN));
+        setFadeAnimation();
+
+        SharedPreferences.Editor editor = getSharedPreferences("check", MODE_PRIVATE).edit();
+        editor.putString("firstTime","N");
+        editor.apply();
+    }
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+}
--- CookandShare/app/src/main/java/com/example/huynhha/cookandshare/MainActivity.java
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.huynhha.cookandshare;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        SharedPreferences prefs = getSharedPreferences("check", MODE_PRIVATE);
+        String restoredText = prefs.getString("firstTime", null);
+        if (restoredText != "N") {
+            Intent intent=new Intent(MainActivity.this,IntroActivity.class);
+            startActivity(intent);
+        }
     }
 }
