7520072024dcd7326570eec46572f80d08f06a92
DESKTOP-H31AHVD\LucTT <lucttse04138@fpt.edu.vn>
2018-05-27 11:16:50 +0700

    tablayout

--- CookandShare/app/src/main/java/com/example/huynhha/cookandshare/MainActivity.java
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.huynhha.cookandshare;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
