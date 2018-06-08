a1d138fe5e6c1a4cc7cefc4c345f805a702a93ae
Huynh Ha <hahpse04181@fpt.edu.vn>
2018-06-09 01:52:30 +0700

    no message

---
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/model/DiffcultDialog.java
@@ -0,0 +1,37 @@
+package com.example.huynhha.cookandshare.model;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.huynhha.cookandshare.R;
+
+public class DiffcultDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog_difficult, null);
+
+        alertDialog.setView(view)
+                .setTitle("Difficult").
+                setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return alertDialog.create();
+    }
+
+}
