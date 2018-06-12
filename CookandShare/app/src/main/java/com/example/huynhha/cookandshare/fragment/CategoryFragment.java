d56819a3598b5c2bb9e7072a6ba632eeb6ec6f75
DESKTOP-H31AHVD\LucTT <lucttse04138@fpt.edu.vn>
2018-06-12 18:11:20 +0700

    Tab layout Categories and Tips

--- CookandShare/app/src/main/java/com/example/huynhha/cookandshare/fragment/CategoryFragment.java
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/fragment/CategoryFragment.java
@@ -2,20 +2,34 @@ package com.example.huynhha.cookandshare.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.huynhha.cookandshare.BuildConfig;
 import com.example.huynhha.cookandshare.R;
+import com.example.huynhha.cookandshare.adapter.PagerAdapter;
+import com.example.huynhha.cookandshare.adapter.SubTabAdapter;
+import com.marlonmafra.android.widget.SegmentedTab;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CategoryFragment extends Fragment {
-
+    SegmentedTab segmentedTab;
+    ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ic_home,
+            R.drawable.ic_categories,
+    };
 
     public CategoryFragment() {
         // Required empty public constructor
@@ -27,9 +41,24 @@ public class CategoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_category, container, false);
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fl_category,new UpdateProfileFragment());
-        transaction.commit();
+        segmentedTab = v.findViewById(R.id.segmentedTabCategories);
+        viewPager = v.findViewById(R.id.viewPagerCategories);
+        setTabLayout();
         return v;
+    }
+
+    private void setupTabIcons() {
+        segmentedTab.getTabAt(0).setIcon(tabIcons[0]);
+        segmentedTab.getTabAt(1).setIcon(tabIcons[1]);
+    }
+    private void setTabLayout() {
+        SubTabAdapter subTabAdapter=new SubTabAdapter(getFragmentManager());
+        subTabAdapter.addFragment(new ListAllCategoriesFragment(),"Categories");
+        subTabAdapter.addFragment(new TipFragment(),"Tips");
+        viewPager.setAdapter(subTabAdapter);
+        segmentedTab.setupWithViewPager(viewPager);
+
+
     }
 
 }
--- CookandShare/app/src/main/java/com/example/huynhha/cookandshare/fragment/PersonalFragment.java
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/fragment/PersonalFragment.java
@@ -4,6 +4,7 @@ package com.example.huynhha.cookandshare.fragment;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,12 +19,7 @@ import com.example.huynhha.cookandshare.adapter.PagerAdapter;
  * A simple {@link Fragment} subclass.
  */
 public class PersonalFragment extends Fragment {
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    private int[] tabIcons = {
-            R.drawable.ic_home,
-            R.drawable.ic_categories,
-    };
+
 
     public PersonalFragment() {
         // Required empty public constructor
@@ -34,26 +30,10 @@ public class PersonalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_personal, container, false);
-        tabLayout = v.findViewById(R.id.tabLayoutCategories);
-        viewPager = v.findViewById(R.id.viewPagerCategories);
-        setTabLayout();
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fl_category, new UpdateProfileFragment());
+        transaction.commit();
         return v;
     }
 
-    private void setupTabIcons() {
-        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
-        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
-    }
 
-    private void setTabLayout() {
-        PagerAdapter pagerAdapter = new PagerAdapter(getFragmentManager());
-        //adding fragment
-        pagerAdapter.addFragment(new ListAllCategoriesFragment(), "");
-        pagerAdapter.addFragment(new TipFragment(), "");
-
-        viewPager.setAdapter(pagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-        setupTabIcons();
-        System.out.println("App id : " + BuildConfig.APPLICATION_ID);
-    }
 }
