3a689adef9b41b10f65ca387acc311246233ffa8
Huynh Ha <hahpse04181@fpt.edu.vn>
2018-07-14 09:46:01 +0700

    fix UI

---
+++ CookandShare/app/src/main/java/com/example/huynhha/cookandshare/PostDetails.java
@@ -0,0 +1,14 @@
+package com.example.huynhha.cookandshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+    }
+
+}
